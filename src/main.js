4d1b24cdbd0e9bc80798c763c5d03f9bb3ce62e9
NoergaardMikkelsen <mette.lund@nmic.dk>
2025-09-30 13:16:58 +0200

    feature: vercel analytics

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 // Entry point: mounts the Vue app to the #app element
 import { createApp } from 'vue';
 import App from './App.vue';
-import './assets/global.css'; // Import global styles for the whole app
+import './assets/global.css';
+import { inject } from '@vercel/analytics' 
 
 // Entry point: mounts the Vue app to the #app element
 createApp(App).mount('#app'); 
+inject();
